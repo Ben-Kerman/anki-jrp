97901d0ef7f7e1fcd50a68429ae77a034c7a700e
Ben Kerman <ben@kermanic.org>
2022-01-25 23:29:44 +0100

    Improve MeCab unit parsing

--- mecab.py
+++ mecab.py
@@ -1,20 +1,31 @@
 import re
+import sys
 from subprocess import PIPE, Popen
 
-_unit_re = re.compile(r"(?P<orig>.+)\t"
-                      r"(?P<hinsi>.+),"
-                      r"(?P<hinsi_class_1>.+),"
-                      r"(?P<hinsi_class_2>.+),"
-                      r"(?P<hinsi_class_3>.+),"
-                      r"(?P<conj_type>.+),"
-                      r"(?P<conj_form>.+),"
-                      r"(?P<base_form>.+),"
-                      r"(?P<reading>.+),"
-                      r"(?P<pronunciation>.+)")
+_unit_re = re.compile(r"(?P<hinsi>.+?),"
+                      r"(?P<hinsi_class_1>.+?),"
+                      r"(?P<hinsi_class_2>.+?),"
+                      r"(?P<hinsi_class_3>.+?),"
+                      r"(?P<conj_type>.+?),"
+                      r"(?P<conj_form>.+?),"
+                      r"(?P<base_form>.+?),"
+                      r"(?P<reading>.+?),"
+                      r"(?P<pronunciation>.+?)")
 
 
 class MecabException(Exception):
     pass
+
+
+def raise_ast(val: str) -> str:
+    if val == "*":
+        raise MecabException
+    else:
+        return val
+
+
+def handle_ast(val: str) -> str | None:
+    return None if val == "*" else val
 
 
 class MecabUnit:
@@ -30,20 +41,33 @@ class MecabUnit:
     pronunciation: str | None
 
     def __init__(self, res: str):
-        m = _unit_re.fullmatch(res)
-        if m is None:
-            raise MecabException
+        orig, data = res.split("\t", 1)
+        self.orig = raise_ast(orig)
+        if data == "未知語":
+            self.hinsi = "未知語"
+            self.hinsi_class_1 = None
+            self.hinsi_class_2 = None
+            self.hinsi_class_3 = None
+            self.conj_type = None
+            self.conj_form = None
+            self.base_form = None
+            self.reading = None
+            self.pronunciation = None
         else:
-            self.orig: str = m.group("orig")
-            self.hinsi: str = m.group("hinsi")
-            self.hinsi_class_1: str = m.group("hinsi_class_1")
-            self.hinsi_class_2: str = m.group("hinsi_class_2")
-            self.hinsi_class_3: str = m.group("hinsi_class_3")
-            self.conj_type: str = m.group("conj_type")
-            self.conj_form: str = m.group("conj_form")
-            self.base_form: str = m.group("base_form")
-            self.reading: str = m.group("reading")
-            self.pronunciation: str = m.group("pronunciation")
+            m = _unit_re.fullmatch(data)
+            if m is None:
+                print(f"invalid MeCab unit: {res}", file=sys.stderr)
+                raise MecabException
+            else:
+                self.hinsi = raise_ast(m.group("hinsi"))
+                self.hinsi_class_1 = handle_ast(m.group("hinsi_class_1"))
+                self.hinsi_class_2 = handle_ast(m.group("hinsi_class_2"))
+                self.hinsi_class_3 = handle_ast(m.group("hinsi_class_3"))
+                self.conj_type = handle_ast(m.group("conj_type"))
+                self.conj_form = handle_ast(m.group("conj_form"))
+                self.base_form = raise_ast(m.group("base_form"))
+                self.reading = raise_ast(m.group("reading"))
+                self.pronunciation = raise_ast(m.group("pronunciation"))
 
     def __repr__(self):
         return "MecabUnit[" \
@@ -66,7 +90,7 @@ class Mecab:
         self._inst = None
 
     def _init(self):
-        self._inst = Popen(["mecab"], stdin=PIPE, stdout=PIPE)
+        self._inst = Popen(["mecab", "--unk-feature=未知語"], stdin=PIPE, stdout=PIPE)
 
     def _instance(self) -> Popen:
         if self._inst is None:
@@ -77,5 +101,7 @@ class Mecab:
         inst = self._instance()
         inst.stdin.write(txt.encode("utf-8") + b"\n")
         inst.stdin.flush()
+        units = []
         while (line := inst.stdout.readline().rstrip(b"\r\n")) != b"EOS":
-            print(MecabUnit(line.decode("utf-8")))
+            units.append(MecabUnit(line.decode("utf-8")))
+        return units
