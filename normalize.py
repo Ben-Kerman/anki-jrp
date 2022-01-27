c383fa80b5ceaadfffbced7b73e6c73f68857863
Ben Kerman <ben@kermanic.org>
2022-01-28 00:24:12 +0100

    Add functions for checking kana-only strings

--- normalize.py
+++ normalize.py
@@ -4,8 +4,16 @@ _to_hira_tbl = str.maketrans(_kata, _hira)
 _to_kata_tbl = str.maketrans(_hira, _kata)
 
 
+def is_hiragana(val: str) -> bool:
+    return all(c in _hira for c in val)
+
+
 def to_hiragana(val: str) -> str:
     return val.translate(_to_hira_tbl)
+
+
+def is_katakana(val: str) -> bool:
+    return all(c in _kata for c in val)
 
 
 def to_katakana(val: str) -> str:
