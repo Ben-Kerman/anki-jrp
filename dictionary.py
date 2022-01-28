1592c13d5b8f00e59afbec4ce234a56eb7deffb0
Ben Kerman <ben@kermanic.org>
2022-01-29 00:58:07 +0100

    Combine accent_dict and reading_dict

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,5 @@
 import lzma
+import os
 import sys
 from typing import TextIO
 
@@ -64,3 +65,31 @@ class AccentDict:
 
     def look_up_reading(self, reading: str) -> list[Entry] | None:
         return self._rdng_idx.get(reading)
+
+
+class ReadingDict:
+    _variants: dict[str, list[str]]
+
+    def __init__(self, dir):
+        self._variants = {}
+
+        with os.scandir(dir) as itr:
+            e: os.DirEntry
+            for file in (e for e in itr if e.is_file() and os.path.basename(e.path).endswith(".dict.xz")):
+                fd: TextIO
+                with lzma.open(file.path, "rt", encoding="utf-8") as fd:
+                    for line in (rl.rstrip("\r\n") for rl in fd):
+                        if line.startswith("#"):
+                            continue
+
+                        try:
+                            variant, reading = [sys.intern(s) for s in line.split("\t")]
+                            if variant not in self._variants:
+                                self._variants[variant] = [reading]
+                            elif reading not in self._variants[variant]:
+                                self._variants[variant].append(reading)
+                        except ValueError:
+                            warn(f"skipping invalid dict entry: {line}")
+
+    def look_up(self, val: str):
+        return self._variants[val]
