63b43440cb14c692ff946fddb443c8048ca86161
Ben Kerman <ben@kermanic.org>
2022-01-29 22:44:28 +0100

    Improve Migaku accent dict import
    Merge entries with identical reading and pitch
    Convert all readings to hiragana
    Sort entries by reading

--- import_migaku_pitch.py
+++ import_migaku_pitch.py
@@ -4,49 +4,74 @@ import json
 import lzma
 import sys
 
+from normalize import to_hiragana
+from util import warn
+
 
 class Entry:
-    word: str
     reading: str
+    variants: list[str]
     accents: list[int]
-    source: str
+    sources: list[str]
 
-    def __init__(self, word: str, reading: str, accents: list[list[int]], source: str):
-        self.word = word
-        self.reading = reading
+    def __init__(self, reading: str, variant: str, accents: list[list[int]], source: str):
+        self.reading = to_hiragana(reading)
+        self.variants = [variant]
         self.accents = [a for inner in accents for a in inner]
         match source:
             case "nhk":
-                self.source = "日"
+                self.sources = ["日"]
             case "daiji":
-                self.source = "林"
+                self.sources = ["林"]
             case "mia":
-                self.source = "Ｍ"
+                self.sources = ["Ｍ"]
             case "wa":
-                self.source = "和"
+                self.sources = ["和"]
             case "shin":
-                self.source = "新"
+                self.sources = ["新"]
+            case _:
+                warn(f"unknown source: {source}")
 
     def fmt_line(self):
-        return f"{self.word}\t{self.reading}\t{','.join(map(str, self.accents))}\t{self.source}\n"
+        return f"{self.reading}\t" \
+               f"{','.join(self.variants)}\t" \
+               f"{','.join(map(str, self.accents))}\t" \
+               f"{''.join(self.sources)}\n"
 
 
-entries = []
-sources = set()
+entries: list[Entry] = []
+readings: dict[str, list[Entry]] = {}
+sources: set[str] = set()
 
 for src_path in sys.argv[2:]:
     with open(src_path, encoding="utf-8") as sfd:
         src_dict = json.load(sfd)
 
     for src_entry in src_dict:
+        if "," in src_entry[0]:
+            warn(f"skipping variant containing comma: {src_entry[0]}")
+            continue
+
         sources.add(src_entry[7])
-        entries.append(Entry(src_entry[0], src_entry[1], src_entry[5], src_entry[7]))
+        entry = Entry(src_entry[1], src_entry[0], src_entry[5], src_entry[7])
+        same_reading = readings.setdefault(entry.reading, [])
+        if same_accent := next((e for e in same_reading if set(entry.accents) == set(e.accents)), None):
+            if entry.variants not in same_accent.variants:
+                same_accent.variants.extend(entry.variants)
+                same_accent.variants.sort()
+            same_accent.sources.extend(set(entry.sources) - set(same_accent.sources))
+            same_accent.sources.sort()
+        else:
+            entries.append(entry)
+            same_reading.append(entry)
+
+entries.sort(key=lambda e: e.reading)
 
 with lzma.open(sys.argv[1], "wt", encoding="utf-8") as tfd:
     tfd.write("# compiled pitch accent dictionary by Yoga, taken from the Migaku Japanese addon\n")
     tfd.write("# sources: 日: NHKアクセント 1998版; 林: 大辞林 第二版; Ｍ: manual changes from MIA; 和: 和独辞典; 新: 新明解 第五版\n")
-    tfd.write("# format: word<TAB>reading<TAB>accent{,accent}<TAB>source\n")
+    tfd.write("# format: reading<TAB>variant{,variant}<TAB>accent{,accent}<TAB>source{,source}\n")
     for entry in entries:
         tfd.write(entry.fmt_line())
 
-print(sources, file=sys.stderr)
+print("sources:", sources, file=sys.stderr)
