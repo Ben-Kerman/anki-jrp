c304f14e81420b57c4a3a5efe7b5f667df7d4c92
Ben Kerman <ben@kermanic.org>
2022-01-29 20:19:07 +0100

    Match accent/variant dict

--- dictionary.py
+++ dictionary.py
@@ -33,33 +33,33 @@ class AccentEntry:
 
 class AccentDict:
     _entries: list[AccentEntry]
-    _word_idx: dict[str, list[AccentEntry]]
-    _rdng_idx: dict[str, list[AccentEntry]]
+    _variants: dict[str, list[AccentEntry]]
+    _readings: dict[str, list[AccentEntry]]
 
     def __init__(self, path):
         self._entries = []
-        self._word_idx = {}
-        self._rdng_idx = {}
+        self._variants = {}
+        self._readings = {}
 
         fd: TextIO
         with lzma.open(path, "rt", encoding="utf-8") as fd:
-            for line in (rl.strip() for rl in fd):
-                if line.lstrip().startswith("#"):
+            for line in (rl.rstrip("\r\n") for rl in fd):
+                if line.startswith("#"):
                     continue
 
                 try:
                     entry = AccentEntry.from_line(line)
                     self._entries.append(entry)
-                    self._word_idx.setdefault(entry.word, []).append(entry)
-                    self._rdng_idx.setdefault(entry.reading, []).append(entry)
+                    self._variants.setdefault(entry.word, []).append(entry)
+                    self._readings.setdefault(entry.reading, []).append(entry)
                 except ValueError:
                     warn(f"skipping invalid dict entry: {line}")
 
-    def look_up_word(self, word: str) -> list[AccentEntry] | None:
-        return self._word_idx.get(word)
+    def look_up_variant(self, word: str) -> list[AccentEntry] | None:
+        return self._variants.get(word)
 
     def look_up_reading(self, reading: str) -> list[AccentEntry] | None:
-        return self._rdng_idx.get(reading)
+        return self._readings.get(reading)
 
 
 class VariantEntry:
@@ -83,28 +83,25 @@ class VariantDict:
     _readings: dict[str, list[VariantEntry]]
     _variants: dict[str, list[VariantEntry]]
 
-    def __init__(self, dir):
+    def __init__(self, path):
         self._entries = []
         self._variants = {}
         self._readings = {}
 
-        with os.scandir(dir) as itr:
-            e: os.DirEntry
-            for file in (e for e in itr if e.is_file() and os.path.basename(e.path).endswith(".dict.xz")):
-                fd: TextIO
-                with lzma.open(file.path, "rt", encoding="utf-8") as fd:
-                    for line in (rl.rstrip("\r\n") for rl in fd):
-                        if line.startswith("#"):
-                            continue
+        fd: TextIO
+        with lzma.open(path, "rt", encoding="utf-8") as fd:
+            for line in (rl.rstrip("\r\n") for rl in fd):
+                if line.startswith("#"):
+                    continue
 
-                        try:
-                            entry = VariantEntry.from_line(line)
-                            self._entries.append(entry)
-                            self._readings.setdefault(entry.reading, []).append(entry)
-                            for var in entry.variants:
-                                self._variants.setdefault(var, []).append(entry)
-                        except ValueError:
-                            warn(f"skipping invalid dict entry: {line}")
+                try:
+                    entry = VariantEntry.from_line(line)
+                    self._entries.append(entry)
+                    self._readings.setdefault(entry.reading, []).append(entry)
+                    for var in entry.variants:
+                        self._variants.setdefault(var, []).append(entry)
+                except ValueError:
+                    warn(f"skipping invalid dict entry: {line}")
 
     def look_up_variant(self, val: str) -> list[VariantEntry] | None:
         return self._variants.get(val)
@@ -124,15 +121,15 @@ class Dictionary:
     def _find_from_var(self, ve: VariantEntry, reading: str | None = None) -> tuple[str, list[int]] | None:
         rdng = reading or ve.reading
         for var in ve.variants:
-            acc_lookup = self.accent.look_up_word(var)
+            acc_lookup = self.accent.look_up_variant(var)
             if acc_lookup is not None:
                 acc_res = next((e for e in acc_lookup if e.reading == rdng), None)
                 if acc_res is not None:
                     return rdng, acc_res.accents
         return None
 
-    def look_up(self, word: str, candidate_reading: str | None) -> tuple[str, list[int]] | None:
-        by_word = self.accent.look_up_word(word)
+    def look_up(self, word: str, candidate_reading: str | None = None) -> tuple[str, list[int]] | None:
+        by_word = self.accent.look_up_variant(word)
         if by_word is not None:
             ent = next((e for e in by_word if e.reading == candidate_reading), None)
             if ent is not None:
