f327b884d5ee7297e0409e51ab076d307bcec8e7
Ben Kerman <ben@kermanic.org>
2022-02-06 00:34:24 +0100

    Add attribute for applying word overrides before lookup

--- overrides.py
+++ overrides.py
@@ -24,6 +24,7 @@ class WordOverride:
     old_reading: str | None
     new_variants: list[str] | None
     new_reading: str | None
+    before_lookup: bool = False
 
     @classmethod
     def from_json(cls, obj: dict) -> "WordOverride":
@@ -31,9 +32,10 @@ class WordOverride:
         old_reading = check_json_value(obj, "old_reading", str)
         new_variants = check_json_value(obj, "new_variants", list, str)
         new_reading = check_json_value(obj, "new_reading", str)
+        before_lookup = check_json_value(obj, "before_lookup", bool)
         if not new_variants and not new_reading:
             raise ConfigError("new variant list and new reading can't both be missing")
-        return cls(old_variants, old_reading, new_variants, new_reading)
+        return cls(old_variants, old_reading, new_variants, new_reading, before_lookup)
 
 
 @dataclass
