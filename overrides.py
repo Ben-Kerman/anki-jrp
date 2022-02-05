e3e6b1dd294f74a9c8e550bbe55df2cebcb06f5a
Ben Kerman <ben@kermanic.org>
2022-02-06 00:34:55 +0100

    Create word override apply function

--- overrides.py
+++ overrides.py
@@ -1,3 +1,4 @@
+from collections.abc import Generator
 from dataclasses import dataclass
 
 from util import ConfigError, check_json_value
@@ -25,6 +26,13 @@ class WordOverride:
     new_variants: list[str] | None
     new_reading: str | None
     before_lookup: bool = False
+
+    def apply(self, variant: str, reading: str | None) -> Generator[tuple[str, str | None]] | None:
+        if variant in self.old_variants and (not self.old_reading or not reading or reading == self.old_reading):
+            new_variants = self.new_variants or (variant,)
+            return ((nv, self.new_reading or reading) for nv in new_variants)
+        else:
+            return None
 
     @classmethod
     def from_json(cls, obj: dict) -> "WordOverride":
