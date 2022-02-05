fe66399e3af6ce713297e8a2a1bb444deb1b461b
Ben Kerman <ben@kermanic.org>
2022-02-05 21:56:16 +0100

    Create data classes for override settings

---
+++ overrides.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass
+
+from preferences import ConfigError, check_json_value
+
+
+class OverrideError(Exception):
+    pass
+
+
+@dataclass
+class IgnoreOverride:
+    variants: list[str]
+    reading: str | None = None
+
+    @classmethod
+    def from_json(cls, obj: dict) -> "IgnoreOverride":
+        variants = check_json_value(obj, "variants", list, str, True)
+        reading = check_json_value(obj, "reading", str)
+        return cls(variants, reading)
+
+
+@dataclass
+class WordOverride:
+    old_variants: list[str]
+    old_reading: str | None
+    new_variants: list[str] | None
+    new_reading: str | None
+
+    @classmethod
+    def from_json(cls, obj: dict) -> "WordOverride":
+        old_variants = check_json_value(obj, "old_variants", list, str, True)
+        old_reading = check_json_value(obj, "old_reading", str)
+        new_variants = check_json_value(obj, "new_variants", list, str)
+        new_reading = check_json_value(obj, "new_reading", str)
+        if not new_variants and not new_reading:
+            raise ConfigError("new variant list and new reading can't both be missing")
+        return cls(old_variants, old_reading, new_variants, new_reading)
+
+
+@dataclass
+class AccentOverride:
+    variants: list[str]
+    reading: str
+    accents: list[int]
+
+    @classmethod
+    def from_json(cls, obj: dict) -> "AccentOverride":
+        variants = check_json_value(obj, "variants", list, str, True)
+        reading = check_json_value(obj, "reading", str, required=True)
+        accents = check_json_value(obj, "accents", list, int, True)
+        return cls(variants, reading, accents)
--- preferences.py
+++ preferences.py
@@ -1,4 +1,27 @@
 from dataclasses import dataclass
+from typing import Type, TypeVar
+
+from overrides import AccentOverride, IgnoreOverride, WordOverride
+
+
+class ConfigError(ValueError):
+    pass
+
+
+T = TypeVar("T")
+E = TypeVar("E")
+
+
+def check_json_value(obj: dict, name: str,
+                     typ: Type[T], elem_type: Type[E] | None = None,
+                     required: bool = False) -> T | list[E]:
+    val = obj.get(name)
+    if required and val is None:
+        raise ConfigError(f"missing value: '{name}'")
+    print(val, type(val), typ, elem_type)
+    if val and (type(val) != typ or (elem_type and any(type(e) != elem_type for e in val))):
+        raise ConfigError(f"invalid value: '{name}'")
+    return val
 
 
 @dataclass
@@ -21,8 +44,16 @@ class JoinPrefs:
 
 
 @dataclass
+class Overrides:
+    ignore: list[IgnoreOverride]
+    word: list[WordOverride]
+    accent: list[AccentOverride]
+
+
+@dataclass
 class ConvPrefs:
     join: JoinPrefs
+    overrides: Overrides
     prefer_accent_lookups: bool = False
 
 
