8ab06b5f4a032af2b5dd36097e24c100d2469339
Ben Kerman <ben@kermanic.org>
2022-02-10 20:35:44 +0100

    Set pre/post_lookup default values properly

--- overrides.py
+++ overrides.py
@@ -45,8 +45,8 @@ class WordOverride:
         old_reading = check_json_value(obj, "old_reading", str)
         new_variants = check_json_list(obj, "new_variants", str)
         new_reading = check_json_value(obj, "new_reading", str)
-        pre_lookup = check_json_value(obj, "pre_lookup", bool)
-        post_lookup = check_json_value(obj, "post_lookup", bool)
+        pre_lookup = check_json_value(obj, "pre_lookup", bool, default=False)
+        post_lookup = check_json_value(obj, "post_lookup", bool, default=True)
         if not new_variants and not new_reading:
             raise ConfigError("new variant list and new reading can't both be missing")
         return cls(old_variants, old_reading, new_variants, new_reading, pre_lookup, post_lookup)
