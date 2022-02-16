533a08a06e7b819e56b7cd28b37dd649bee26b07
Ben Kerman <ben@kermanic.org>
2022-02-16 23:35:42 +0100

    Simplify from_json()

--- util.py
+++ util.py
@@ -31,76 +31,64 @@ def from_json(json_val: Any, typ: Type[T], try_cls_method: bool = True) -> T | C
     def types_to_list(t: Any) -> tuple:
         return get_args(t) if type(t) == UnionType else (t,)
 
-    def check_type(val: Any, expected: Type[T], actual: Type[U]) -> U | ConfigError:
-        return val if expected == actual else ConfigError(f"invalid type (value: {val})")
+    def check_type(val: Any, t: Type[T]) -> U | ConfigError:
+        return val if type(val) is t else ConfigError(f"invalid type")
 
-    match json_val:
-        case str(s):
-            return check_type(s, typ, str)
-        case bool(b):
-            return check_type(b, typ, bool)
-        case int(i):
-            return check_type(i, typ, int)
-        case float(f):
-            return check_type(f, typ, float)
-        case None:
-            if typ is NoneType:
-                return None
-            else:
-                return ConfigError("missing value")
-        case list(lst):
-            if type(typ) == GenericAlias and get_origin(typ) in (list, set):
-                elem_types = types_to_list(get_args(typ)[0])
+    if typ in (str, bool, int, float, NoneType):
+        return check_type(json_val, typ)
+    elif type(typ) == GenericAlias and get_origin(typ) in (list, set):
+        lst = check_type(json_val, list)
+        if isinstance(lst, ConfigError):
+            return lst
 
-                new_lst: list = []
-                err = None
-                for i, e in enumerate(lst):
-                    for et in elem_types:
-                        res = from_json(e, et)
-                        if isinstance(res, ConfigError):
-                            err = res
-                        else:
-                            new_lst.append(res)
-                            err = None
-                            break
-                    else:
-                        break
+        elem_types = types_to_list(get_args(typ)[0])
 
-                if err:
-                    return ConfigError(f"[{i}]: {err.msg}")
-                if get_origin(typ) is set:
-                    return set(new_lst)
+        new_lst: list = []
+        err = None
+        for i, e in enumerate(lst):
+            for et in elem_types:
+                res = from_json(e, et)
+                if isinstance(res, ConfigError):
+                    err = res
                 else:
-                    return new_lst
-            else:
-                return ConfigError("invalid type")
-        case dict(dic):
-            if try_cls_method:
-                try:
-                    return typ.from_json(dic)
-                except AttributeError:
-                    pass
-
-            init = {}
-            for field in dataclasses.fields(typ):
-                if field.name not in dic:
-                    if field.default is MISSING and field.default_factory is MISSING:
-                        return ConfigError("missing value")
-                    else:
-                        continue
-                else:
-                    field_val = dic[field.name]
+                    new_lst.append(res)
                     err = None
-                    for t in types_to_list(field.type):
-                        res = from_json(field_val, t)
-                        if isinstance(res, ConfigError):
-                            err = res
-                        else:
-                            init[field.name] = res
-                            err = None
-                            break
-                    if err:
-                        return ConfigError(f"{field.name}: {err.msg}")
-            return typ(**init)
-        case _:
+                    break
+            else:
+                break
+
+        if err:
+            return ConfigError(f"[{i}]: {err.msg}")
+        return set(new_lst) if get_origin(typ) is set else new_lst
+    else:
+        dic = check_type(json_val, dict)
+        if isinstance(dic, ConfigError):
             return ConfigError("impossible JSON value")
+
+        if try_cls_method:
+            try:
+                return typ.from_json(dic)
+            except AttributeError:
+                pass
+
+        init = {}
+        for field in dataclasses.fields(typ):
+            if field.name not in dic:
+                if field.default is MISSING and field.default_factory is MISSING:
+                    return ConfigError("missing value")
+                else:
+                    continue
+            else:
+                field_val = dic[field.name]
+                err = None
+                for ft in types_to_list(field.type):
+                    res = from_json(field_val, ft)
+                    if isinstance(res, ConfigError):
+                        err = res
+                    else:
+                        init[field.name] = res
+                        err = None
+                        break
+                if err:
+                    return ConfigError(f"{field.name}: {err.msg}")
+        return typ(**init)
