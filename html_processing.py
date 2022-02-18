2c2a1ac76f8adc18e5dd48a161ae7af50e213514
Ben Kerman <ben@kermanic.org>
2022-02-18 02:49:36 +0100

    Implement basic HTML-to-text converter

---
+++ html_processing.py
@@ -0,0 +1,85 @@
+from html.parser import HTMLParser
+
+_void_tags = {"area", "base", "br", "col", "command", "embed", "hr", "img", "input", "keygen", "link", "meta", "param",
+              "source", "track", "wbr"}
+_block_tags = {"blockquote", "dd", "div", "dl", "dt", "figcaption", "figure", "hr", "li", "ol", "p", "pre", "ul"}
+_break_tags = {"br", "hr"}
+
+_follow_omit_map = {
+    "dd": {"dd", "dt"},
+    "dt": {"dd", "dt"},
+    "li": {"li"},
+    "optgroup": {"optgroup"},
+    "option": {"option", "optgroup"},
+    "p": {"address", "article", "aside", "blockquote", "details", "div", "dl", "fieldset", "figcaption", "figure",
+          "footer", "form", "h1", "h2", "h3", "h4", "h5", "h6", "header", "hgroup", "hr", "main", "menu", "nav", "ol",
+          "p", "pre", "section", "table", "ul"},
+    "rp": {"rp", "rt"},
+    "rt": {"rp", "rt"},
+    "tbody": {"tbody", "tfoot"},
+    "td": {"td", "th"},
+    "th": {"td", "th"},
+    "thead": {"tbody", "tfoot"},
+    "tr": {"tr"},
+}
+
+_end_omit = {"dd", "li", "optgroup", "option", "p", "rp", "rt", "tbody", "td", "tfoot", "th", "tr"}
+_p_no_end_omit = {"a", "audio", "del", "ins", "map", "noscript", "video"}
+
+
+class JrpHTMLParser(HTMLParser):
+    tag_stack: list[str]
+    lines: list[str]
+    cur_line: list[str]
+
+    def __init__(self):
+        super().__init__()
+        self.tag_stack = []
+        self.lines = []
+        self.cur_line = []
+
+    def _insert_line(self):
+        self.lines.append("".join(self.cur_line))
+        self.cur_line = []
+
+    def handle_starttag(self, tag: str, attrs: list[tuple[str, str | None]]) -> None:
+        def handle_omission(tag_stack: list[str], start_tag: str) -> str | None:
+            if not tag_stack:
+                return None
+            stack_tag: str = tag_stack[-1]
+            if stack_tag in _follow_omit_map and start_tag in _follow_omit_map[stack_tag]:
+                return tag_stack.pop()
+
+        omitted_tag = handle_omission(self.tag_stack, tag)
+        if omitted_tag and omitted_tag in _block_tags or tag in _block_tags or tag in _break_tags:
+            self._insert_line()
+        if tag not in _void_tags:
+            self.tag_stack.append(tag)
+
+    def handle_endtag(self, tag: str) -> None:
+        def handle_omission(tag_stack: list[str], end_tag: str) -> str | None:
+            if not tag_stack:
+                return None
+            stack_tag: str = tag_stack[-1]
+            if end_tag != stack_tag and stack_tag in _end_omit:
+                if stack_tag != "p" or (end_tag not in _p_no_end_omit):
+                    return tag_stack.pop()
+            return None
+
+        omitted_tag = handle_omission(self.tag_stack, tag)
+        if tag in _block_tags or omitted_tag and omitted_tag in _block_tags:
+            self._insert_line()
+        if self.tag_stack and self.tag_stack[-1] == tag:
+            self.tag_stack.pop()
+
+    def handle_data(self, data: str) -> None:
+        self.cur_line.append(data)
+
+    def close(self) -> list[str]:
+        super().close()
+        self._insert_line()
+        lines: list[str] = []
+        for line in self.lines:
+            if stripped := line.strip():
+                lines.append(stripped)
+        return lines
