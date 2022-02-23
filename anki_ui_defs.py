44c11b8e07c1e96249fa20f091dc4fb6c62ee4ae
Ben Kerman <ben@kermanic.org>
2022-02-24 00:06:16 +0100

    Rearrange GUI defs

--- anki_ui_defs.py
+++ anki_ui_defs.py
@@ -1,68 +1,54 @@
 conv_checkboxes = [
     "General conversion settings",
     {
-        "desc": "Prefer adding accents over readings",
-        "path": ("convert", "prefer_accent_lookups")
+        "path": ("convert", "prefer_accent_lookups"),
+        "desc": "Prefer adding accents over readings"
     },
     "Join preferences for 動詞・形容詞",
     {
-        "desc": "Join ない（知らない・早くない）",
-        "path": ("convert", "join", "yougen_join_nai")
-    },
-    {
-        "desc": "Join う（知ろう・早かろう）",
-        "path": ("convert", "join", "yougen_join_u")
-    },
-    {
-        "desc": "Join た（知った・早かった）",
-        "path": ("convert", "join", "yougen_join_ta")
-    },
-    {
-        "desc": "Join て（知って・早くて）",
-        "path": ("convert", "join", "yougen_join_te")
-    },
-    {
-        "desc": "Join ば（知れば・早ければ）",
-        "path": ("convert", "join", "yougen_join_ba")
-    },
-    {
-        "desc": "Join そう（知りそう・早そう）",
-        "path": ("convert", "join", "yougen_join_sou")
-    },
-    {
-        "desc": "Join さ（早さ）",
-        "path": ("convert", "join", "keiyousi_join_sa")
-    },
-    {
-        "desc": "Join たい（知りたい）",
-        "path": ("convert", "join", "dousi_join_tai")
-    },
-    {
-        "desc": "Join ぬ（知らぬ）",
-        "path": ("convert", "join", "dousi_join_nu")
-    },
-    {
-        "desc": "Join ん（知らん）",
-        "path": ("convert", "join", "dousi_join_n")
-    },
-    {
-        "desc": "Join れる（知られる）",
-        "path": ("convert", "join", "dousi_join_reru")
-    },
-    {
-        "desc": "Join せる（知らせる）",
-        "path": ("convert", "join", "dousi_join_seru")
-    },
-    {
-        "desc": "Join ます（知ります）",
-        "path": ("convert", "join", "dousi_join_masu")
-    },
-    {
-        "desc": "Join ちゃう（知っちゃう）",
-        "path": ("convert", "join", "dousi_join_tyau")
-    },
-    {
-        "desc": "Split てる（知って・る）",
-        "path": ("convert", "join", "dousi_split_teru")
-    },
+        "path": ("convert", "join", "yougen_join_nai"),
+        "desc": "Join ない（知らない・早くない）"
+    }, {
+        "path": ("convert", "join", "yougen_join_u"),
+        "desc": "Join う（知ろう・早かろう）"
+    }, {
+        "path": ("convert", "join", "yougen_join_ta"),
+        "desc": "Join た（知った・早かった）"
+    }, {
+        "path": ("convert", "join", "yougen_join_te"),
+        "desc": "Join て（知って・早くて）"
+    }, {
+        "path": ("convert", "join", "yougen_join_ba"),
+        "desc": "Join ば（知れば・早ければ）"
+    }, {
+        "path": ("convert", "join", "yougen_join_sou"),
+        "desc": "Join そう（知りそう・早そう）"
+    }, {
+        "path": ("convert", "join", "keiyousi_join_sa"),
+        "desc": "Join さ（早さ）"
+    }, {
+        "path": ("convert", "join", "dousi_join_tai"),
+        "desc": "Join たい（知りたい）"
+    }, {
+        "path": ("convert", "join", "dousi_join_nu"),
+        "desc": "Join ぬ（知らぬ）"
+    }, {
+        "path": ("convert", "join", "dousi_join_n"),
+        "desc": "Join ん（知らん）"
+    }, {
+        "path": ("convert", "join", "dousi_join_reru"),
+        "desc": "Join れる（知られる）"
+    }, {
+        "path": ("convert", "join", "dousi_join_seru"),
+        "desc": "Join せる（知らせる）"
+    }, {
+        "path": ("convert", "join", "dousi_join_masu"),
+        "desc": "Join ます（知ります）"
+    }, {
+        "path": ("convert", "join", "dousi_join_tyau"),
+        "desc": "Join ちゃう（知っちゃう）"
+    }, {
+        "path": ("convert", "join", "dousi_split_teru"),
+        "desc": "Split てる（知って・る）"
+    }
 ]
