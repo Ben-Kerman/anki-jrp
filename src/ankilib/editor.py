5f4371da626186b47aa7c9f52371b833793b2024
Ben Kerman <ben@kermanic.org>
2022-02-27 04:37:20 +0100

    Implement basic Anki editor buttons

--- src/ankilib/editor.py
+++ src/ankilib/editor.py
@@ -1,6 +1,14 @@
 from collections.abc import Callable
+from enum import Enum, auto
 
-from aqt import editor, gui_hooks
+import aqt.utils
+from aqt.editor import Editor
+
+from . import global_vars
+from ..pylib.converter import convert
+from ..pylib.mecab import Mecab
+from ..pylib.output import fmt_jrp, fmt_migaku
+from ..pylib.segments import parse_jrp, parse_migaku
 
 _js_esc = str.maketrans({
     "\r": "\\r",
@@ -10,9 +18,19 @@ _js_esc = str.maketrans({
 })
 
 
-def _replace(edit: editor.Editor, transform: Callable[[str], str]):
+class OutputType(Enum):
+    DEFAULT = auto()
+    MIGAKU = auto()
+
+
+class ConversionType(Enum):
+    GENERATE = auto()
+    READINGS = auto()  # TODO implement
+    REMOVE = auto()
+
+
+def _replace(edit: Editor, transform: Callable[[str], str]):
     def inject_html(new_html: str):
-        nonlocal edit
         edit.web.page().runJavaScript(f"""
         editable = getCurrentField().activeInput
         editable.fieldHTML = \"{transform(new_html).translate(_js_esc)}\"
@@ -21,8 +39,57 @@ def _replace(edit: editor.Editor, transform: Callable[[str], str]):
     edit.web.page().runJavaScript("getCurrentField().activeInput.fieldHTML", lambda html: inject_html(html))
 
 
-def _inject_shortcuts(shortcuts: list[tuple], edit: editor.Editor):
-    shortcuts.append(("F2", lambda: _replace(edit, lambda h: f"〜{h}〜")))
+def _convert(edit: Editor, out_type: OutputType, conv_type: ConversionType):
+    def transform(val: str) -> str:
+        parser = parse_migaku if out_type == OutputType.MIGAKU else parse_jrp
+        units = parser(val)
+
+        text = "".join(u.text() for u in units)
+        if conv_type == ConversionType.REMOVE:
+            return text
+
+        prefs = global_vars.prefs
+        dic = global_vars.dictionary
+        if not dic:
+            aqt.utils.showWarning("Dictionary is not (yet) loaded, can't convert")
+
+        converted = convert(text, prefs.convert, Mecab(), dic)
+
+        formatter = fmt_migaku if out_type == OutputType.MIGAKU else fmt_jrp
+        return formatter(converted)
+
+    _replace(edit, transform)
 
 
-gui_hooks.editor_did_init_shortcuts.append(_inject_shortcuts)
+def inject_buttons(buttons: list[str], edit: Editor):
+    buttons.append(edit.addButton(
+        icon=None,
+        cmd="jrp_generate_default",
+        func=lambda e: _convert(e, OutputType.DEFAULT, ConversionType.GENERATE),
+        tip="[F2] (Re)generate readings and accents with default syntax.",
+        keys="F2")
+    )
+    buttons.append(edit.addButton(
+        icon=None,
+        cmd="jrp_remove_default",
+        func=lambda e: _convert(e, OutputType.DEFAULT, ConversionType.REMOVE),
+        tip="[Ctrl+F2] Restore original text from default syntax.",
+        keys="Ctrl+F2")
+    )
+    buttons.append(edit.addButton(
+        icon=None,
+        cmd="jrp_generate_migaku",
+        func=lambda e: _convert(e, OutputType.MIGAKU, ConversionType.GENERATE),
+        tip="[F3] (re)generate readings and accents with Migaku syntax.",
+        keys="F3")
+    )
+    buttons.append(edit.addButton(
+        icon=None,
+        cmd="jrp_remove_migaku",
+        func=lambda e: _convert(e, OutputType.MIGAKU, ConversionType.REMOVE),
+        tip="[Ctrl+F3] Restore original text from Migaku syntax.",
+        keys="Ctrl+F3")
+    )
+
+
+aqt.gui_hooks.editor_did_init_buttons.append(inject_buttons)
