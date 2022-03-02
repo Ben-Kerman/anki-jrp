6471989660f3f357461a5773dab11f4ae053d21f
Ben Kerman <ben@kermanic.org>
2022-03-03 00:29:52 +0100

    Implement basic addon update procedure

--- src/ankilib/hooks.py
+++ src/ankilib/hooks.py
@@ -1,8 +1,11 @@
 import aqt
 
-from . import browser, editor, global_vars, main_menu
+from . import browser, editor, global_vars, main_menu, updates
+
+updates.update()
 
 aqt.gui_hooks.collection_did_load.append(global_vars.load_prefs)
+aqt.gui_hooks.collection_did_load.append(updates.update_collection)
 aqt.gui_hooks.main_window_did_init.append(main_menu.insert_menu_items)
 aqt.gui_hooks.editor_did_init_buttons.append(editor.inject_buttons)
 aqt.gui_hooks.browser_menus_did_init.append(browser.insert_menu_items)
---
+++ src/ankilib/updates.py
@@ -0,0 +1,42 @@
+import os.path
+
+from anki.collection import Collection
+
+from . import global_vars
+from .templates import update_all_note_types
+from .util import copy_assets, get_path
+from ..pylib import version
+
+
+def _path() -> str:
+    return get_path("user_files", "last-version")
+
+
+def _col_path(col: Collection) -> str:
+    return os.path.join(os.path.dirname(col.path), "jrp-last-version")
+
+
+def check_update(col: Collection | None = None) -> bool:
+    path = _col_path(col) if col else _path()
+    if os.path.exists(path):
+        with open(path) as fd:
+            return fd.read().strip() != version.script
+    return True
+
+
+def update() -> None:
+    if not check_update():
+        return
+
+    with open(_path(), "w") as fd:
+        fd.write(version.script)
+
+
+def update_collection(col: Collection) -> None:
+    if not check_update(col):
+        return
+
+    copy_assets(col)
+    update_all_note_types(col, global_vars.prefs.addon)
+    with open(_col_path(col), "w") as fd:
+        fd.write(version.script)
