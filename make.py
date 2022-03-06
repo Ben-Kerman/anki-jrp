d7c56daaaf74230c1eafe1f2f3dc7fe25c7116b0
Ben Kerman <ben@kermanic.org>
2022-03-06 02:20:51 +0100

    Add builds with data, binaries and IPAdic included

--- make.py
+++ make.py
@@ -2,9 +2,16 @@
 
 import os
 import shutil
+import sys
 from collections.abc import Sequence
 from datetime import datetime
 from zipfile import ZIP_LZMA, ZipFile, ZipInfo
+
+if len(sys.argv) < 3:
+    sys.exit("invalid number of arguments; usage: ./make.py <DATA DIR> <WINDOWS BIN DIR> [<IPADIC DIR>]")
+
+_, data_dir, windows_bin_dir = sys.argv[:3]
+ipadic_dir = sys.argv[3] if len(sys.argv) >= 4 else None
 
 version_dict = {}
 with open("src/pylib/version.py") as vfd:
@@ -35,16 +42,28 @@ def add_files(zf: ZipFile, src_path: str, tgt_path: str | None = None, ext: Sequ
             add_file(zf, path, zpath)
 
 
-def make_addon(path: str, full: bool):
+def make_addon(path: str, full: bool, dic: bool = False):
     with ZipFile(path, "w") as azip:
         for dirname in ("pylib", "ankilib", "assets", "style"):
             add_files(azip, os.path.join("src", dirname), dirname, (".py", ".svg", ".css", ".pat", ".var"))
         add_files(azip, "src/ts", "js", (".js",))
         add_file(azip, "src/__init__.py", "__init__.py")
 
+        if full:
+            add_file(azip, os.path.join(data_dir, "accents.xz"), "data/accents.xz", compress=False)
+            add_file(azip, os.path.join(data_dir, "variants.xz"), "data/variants.xz", compress=False)
+
+            add_file(azip, os.path.join(windows_bin_dir, "libmecab.dll"), "bin/libmecab.dll")
+            add_file(azip, os.path.join(windows_bin_dir, "mecab.exe"), "bin/mecab.exe")
+
+        if dic:
+            add_files(azip, ipadic_dir, "data/ipadict")
+
 
 make_addon(f"target/{name}_{version}.ankiaddon", full=False)
 make_addon(f"target/{name}_{version}_full.ankiaddon", full=True)
+if ipadic_dir:
+    make_addon(f"target/{name}_{version}_ipadic.ankiaddon", full=True, dic=True)
 
 with ZipFile(f"target/{name}_script-only_{version}.zip", "w") as szip:
     add_files(szip, "src/pylib", ext=(".py",))
